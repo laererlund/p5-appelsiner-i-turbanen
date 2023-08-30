1201c6894a1a82cbddda55e63649b59e415e7162
laererlund <laererlund@gmail.com>
2023-08-30 14:14:50 +0200

    Frugtklasse og tilretninger

--- kurv.js
+++ kurv.js
@@ -8,46 +8,61 @@
  * ud fra dem. Værdierne huskes som hørende til netop 
  * dette objekt ved hjælp af nøgleordet this
  */
-function Kurv(x, y, bredde, dybde, speed) {
-    
+class Kurv {
+  constructor(x, y, bredde, dybde, speed) {
+
     this.x = x;
     this.y = y;
     this.bred = bredde;
     this.dyb = dybde;
     this.speed = speed;
-    this.col = [250,230,150];
+    this.col = [250, 230, 150];
 
     /*
-     * Derpå defineres tre funktioner, som kan kaldes på de enkelte instanser 
+     * Derpå defineres nogle funktioner, som kan kaldes på de enkelte instanser
      * (objekter) af klassen. Bemærk at syntaksen er anderledes end i sketch-scriptet
-     * 
-     * Hvad mon disse tre funktioner gør, hver især? 
-     * Tilføj kommentarer, der fortæller det.
-     * Kommenter også selve koden, især mht. de parametre, som funktionerne modtager
      */
-    this.tegn = function() {
-        fill(this.col);
-        rect(this.x, this.y, this.bred, this.dyb);
-    }
 
-    this.move = function(tast) {
-        if (tast == 'w' || tast== 'W') {
-            this.y -= this.speed;
-            if (this.y < 0) {this.y = 0};
-        }
-        if (tast == 's' || tast == 'S') {
-            this.y += this.speed;
-            if (this.y > height-this.dyb) {this.y = height - this.dyb};
-        }
-    }
+    /*
+     * tegn() sørger for, at objektet bliver tegnet på canvas. 
+     * Her kan evt. indsættes et billede
+     */
+    this.tegn = function () {
+      fill(this.col);
+      rect(this.x, this.y, this.bred, this.dyb);
+    };
+    /*
+     * flyt() sørger for at flytte objektet. Her kan bruges enten pile eller WASD
+     */
+    this.flyt = function () {
+      if (keyIsDown(65) || keyIsDown(LEFT_ARROW)) {
+        this.x -= this.speed;
+      }
 
-    this.grebet = function(xa, ya, ra) {
-        if ((ya < this.y+3 && ya > this.y-3) && xa > this.x+ra && xa < this.x+this.bred-ra) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
+      if (keyIsDown(68) || keyIsDown(RIGHT_ARROW)) {
+        this.x += this.speed;
+      }
 
+      if (keyIsDown(87) || keyIsDown(UP_ARROW)) {
+        this.y -= this.speed;
+      }
+
+      if (keyIsDown(83) || keyIsDown(DOWN_ARROW)) {
+        this.y += this.speed;
+      }
+    };
+
+    /*
+     * grebet() tjekker om objektet, der sendes som parameter, har position inden for hitboksen
+     */
+    this.grebet = function (frugt) {
+      if ((frugt.y < this.y + 9 && frugt.y > this.y - 2) && frugt.x > this.x + frugt.bred/3 && frugt.x < this.x + this.bred - frugt.bred/3) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    };
+
+  }
 } 
